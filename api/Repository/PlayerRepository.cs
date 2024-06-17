66f23df4fa04c5de27d48b83999eddc22826c81e
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-17 10:57:58 +0100

    Created DB context classes in Repository folder

--- api/Repository/PlayerRepository.cs
+++ api/Repository/PlayerRepository.cs
@@ -2,16 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Data;
 using api.Interfaces;
 using api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Repository
 {
     public class PlayerRepository : IPlayerRepository
     {
+        private readonly ApplicationDBContext _context;
+        public PlayerRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
         public Task<List<Player>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return _context.Players.ToListAsync();
         }
     }
 }
