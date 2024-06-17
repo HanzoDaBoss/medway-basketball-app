6543365227ce93ef30ad9ee11dfea8a43882b5db
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-17 10:42:59 +0100

    Added PlayerRepository

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b019f685ba62e01c78718f3a6ba8ba8197b636")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa72bdb0eb265cd15a8919e2880d2a66775b5b52")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
