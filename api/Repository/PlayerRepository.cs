a732cf02525372a0b16cdce3c18b6445b9599678
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-17 11:18:30 +0100

    Added CreateAsync method to PlayerRepository class

--- api/Repository/PlayerRepository.cs
+++ api/Repository/PlayerRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Player;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -16,9 +17,32 @@ namespace api.Repository
         {
             _context = context;
         }
+
+        public async Task<Player> CreateAsync(Player playerModel)
+        {
+            await _context.Players.AddAsync(playerModel);
+            await _context.SaveChangesAsync();
+            return playerModel;
+        }
+
+        public Task<Player?> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<List<Player>> GetAllAsync()
         {
             return _context.Players.ToListAsync();
+        }
+
+        public Task<Player?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player?> UpdateAsync(int id, UpdatePlayerRequestDto playerDto)
+        {
+            throw new NotImplementedException();
         }
     }
 }
