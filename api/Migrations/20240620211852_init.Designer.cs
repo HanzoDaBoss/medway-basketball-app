e8e0aef2fa558342b1e3b49bc6930da0b245322a
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-20 22:34:39 +0100

    Reset database and added new migration

--- api/Migrations/20240620211852_init.Designer.cs
+++ api/Migrations/20240620211852_init.Designer.cs
@@ -12,8 +12,8 @@ using api.Data;
 namespace api.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    [Migration("20240620202032_Identity")]
-    partial class Identity
+    [Migration("20240620211852_init")]
+    partial class init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfbe8fc4e2e4e0cd63591b951f98df55628a805b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5088e38cf597791f17d996b26401b61625188f39")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
