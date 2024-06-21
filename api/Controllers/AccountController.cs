e2f00ab645af1855d311bf79086d511ff501beed
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-21 19:53:06 +0100

    Created the methods for adding user to database in AccountController

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Account;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -20,7 +21,41 @@ namespace api.Controllers
         [HttpPost("register")]
         public async Task<ActionResult> Register([FromBody] RegisterDto registerDto)
         {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
 
+                var user = new User
+                {
+                    UserName = registerDto.Username,
+                    Email = registerDto.Email,
+
+                };
+
+                var createdUser = await _userManager.CreateAsync(user, registerDto.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(user, "User");
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok("User created");
+                    }
+                    else
+                    {
+                        return StatusCode(500, roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return StatusCode(500, createdUser.Errors);
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e);
+            }
         }
     }
 }
