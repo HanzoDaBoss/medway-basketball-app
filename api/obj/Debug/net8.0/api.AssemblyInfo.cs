7f74d56e724809d9d0ae1bd54a424f0c59fb32d2
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-21 18:40:46 +0100

    Created AccountController

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5088e38cf597791f17d996b26401b61625188f39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8e0aef2fa558342b1e3b49bc6930da0b245322a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
