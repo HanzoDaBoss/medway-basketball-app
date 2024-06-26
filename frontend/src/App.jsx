c2f3c434dfce88f39d515b9dce8857cc2b5b879b
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-26 17:48:41 +0100

    Created Leaderboard component and added to route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import {useState} from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import {Route, Routes} from "react-router-dom";
+import Leaderboard from "./components/Leaderboard";
 
 function App() {
   return (
     <>
-      <header>Yo</header>
+      <Routes>
+        <Route path="/" element={<Leaderboard />}></Route>
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/components/Leaderboard.jsx
@@ -0,0 +1,8 @@
+export default function Leaderboard() {
+  return (
+    <>
+      <header>hi</header>
+      <h2>This is the leaderboard</h2>
+    </>
+  );
+}
