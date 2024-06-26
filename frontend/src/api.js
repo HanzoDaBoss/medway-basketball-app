c4b3d744acb400fad83c4d3c08392351bf5c71ff
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-26 18:08:28 +0100

    Created api for getPlayers

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,3 +3,21 @@ import axios from "axios";
 const api = axios.create({
   baseURL: `https://medway-basketball.azurewebsites.net/api`,
 });
+
+const getPlayers = (sort_by) => {
+  return api
+    .get(`/players`, {
+      params: {
+        sortBy: sort_by,
+      },
+    })
+    .then(({data}) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) => {
+      return error.response;
+    });
+};
+
+export {getPlayers};
