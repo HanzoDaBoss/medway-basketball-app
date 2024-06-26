c04ef0d4259453026984881bba4859b9905adbb1
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-26 19:25:06 +0100

    Created Leaderboard component which displays all players

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -1,8 +1,38 @@
+import {useEffect, useState} from "react";
+import {getPlayers} from "../api";
+
 export default function Leaderboard() {
+  const [playerList, setPlayerList] = useState([]);
+
+  useEffect(() => {
+    getPlayers("overallRating").then((players) => {
+      setPlayerList(players);
+    });
+  }, []);
+
   return (
     <>
       <header>hi</header>
       <h2>This is the leaderboard</h2>
+      <div>
+        {playerList.map((player) => {
+          return (
+            <div style={{display: "flex", flexDirection: "row", gap: 10}}>
+              <p>{player.playerName}</p>
+              <p>{player.insideScoring}</p>
+              <p>{player.midRangeShooting}</p>
+              <p>{player.longRangeShooting}</p>
+              <p>{player.perimeterDefense}</p>
+              <p>{player.insideDefense}</p>
+              <p>{player.playmaking}</p>
+              <p>{player.rebound}</p>
+              <p>{player.ballHandling}</p>
+              <p>{player.rebound}</p>
+              <p>{player.overallRating}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
