cd5fc9f9dd3e426762562ea96253380da3d9d156
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-29 20:27:53 +0100

    Added bordering to columns to table

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -14,7 +14,7 @@ export default function Leaderboard() {
     <>
       <header>hi</header>
       <h2>This is the leaderboard</h2>
-      <table class="min-w-full text-left text-sm font-light text-surface dark:text-white">
+      <table className="min-w-full border border-neutral-200 text-center text-sm font-light text-surface dark:border-white/10 dark:text-white">
         <thead className="border-b border-neutral-200 font-medium dark:border-white/10">
           <tr>
             <th>Position</th>
@@ -34,17 +34,39 @@ export default function Leaderboard() {
           return (
             <tbody>
               <tr className="border-b border-neutral-200 dark:border-white/10">
-                <td>{index + 1}</td>
-                <td>{player.playerName}</td>
-                <td>{player.insideScoring}</td>
-                <td>{player.midRangeShooting}</td>
-                <td>{player.longRangeShooting}</td>
-                <td>{player.perimeterDefense}</td>
-                <td>{player.insideDefense}</td>
-                <td>{player.playmaking}</td>
-                <td>{player.rebound}</td>
-                <td>{player.ballHandling}</td>
-                <td>{Math.round(player.overallRating)}</td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {index + 1}
+                </td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {player.playerName}
+                </td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {player.insideScoring}
+                </td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {player.midRangeShooting}
+                </td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {player.longRangeShooting}
+                </td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {player.perimeterDefense}
+                </td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {player.insideDefense}
+                </td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {player.playmaking}
+                </td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {player.rebound}
+                </td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {player.ballHandling}
+                </td>
+                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                  {Math.round(player.overallRating)}
+                </td>
               </tr>
             </tbody>
           );
