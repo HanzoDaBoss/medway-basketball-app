ece2e7d8f610f07734c5d96b62669a2dc6798292
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-30 19:58:13 +0100

    Changed column sizing on tabke

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -20,67 +20,67 @@ export default function Leaderboard() {
           <tr>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               #
             </th>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               Name
             </th>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               IS
             </th>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               MRS
             </th>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               LRS
             </th>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               PD
             </th>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               ID
             </th>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               PM
             </th>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               RB
             </th>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               BH
             </th>
             <th
               scope="col"
-              className="border-e border-neutral-200 px-6 py-4 dark:border-white/10"
+              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
             >
               OVR
             </th>
@@ -90,37 +90,37 @@ export default function Leaderboard() {
           return (
             <tbody>
               <tr className="border-b border-neutral-200 dark:border-white/10 ">
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {index + 1}
                 </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {player.playerName}
                 </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {player.insideScoring}
                 </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {player.midRangeShooting}
                 </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {player.longRangeShooting}
                 </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {player.perimeterDefense}
                 </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {player.insideDefense}
                 </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {player.playmaking}
                 </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {player.rebound}
                 </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {player.ballHandling}
                 </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium dark:border-white/10">
+                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
                   {Math.round(player.overallRating)}
                 </td>
               </tr>
