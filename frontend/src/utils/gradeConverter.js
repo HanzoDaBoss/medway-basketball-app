d8951ce8665c9b70443ea942070cd106fe03bf28
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-30 19:20:03 +0100

    Completed gradeConverter function

--- frontend/src/utils/gradeConverter.js
+++ frontend/src/utils/gradeConverter.js
@@ -1,23 +1,25 @@
 function gradeConverter(playerList) {
   const grades = {
-    "A+": 100,
-    A: 95,
-    "A-": 90,
-    "B+": 85,
-    B: 82,
-    "B-": 79,
-    "C+": 74,
-    C: 72,
-    "C-": 69,
-    "D+": 66,
-    D: 62,
-    "D-": 58,
+    100: "A+",
+    95: "A",
+    90: "A-",
+    85: "B+",
+    82: "B",
+    79: "B-",
+    74: "C+",
+    72: "C",
+    69: "C-",
+    66: "D+",
+    62: "D",
+    58: "D-",
   };
 
   playerList.map((player) => {
-    Object.values(player).forEach((attribute, index) => {
+    Object.keys(player).forEach((attribute, index) => {
       if (index > 1 || index < 10) {
+        player[attribute] = grades[attribute];
       }
     });
+    return player;
   });
 }
