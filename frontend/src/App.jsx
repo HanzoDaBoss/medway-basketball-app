b8122eeaecc0f545e4e3b60db01819125da96489
HanzoDaBoss <hanif.uddz@gmail.com>
2024-06-30 20:05:04 +0100

    Added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import {useState} from "react";
 import "./App.css";
 import {Route, Routes} from "react-router-dom";
 import Leaderboard from "./components/Leaderboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Leaderboard />}></Route>
       </Routes>
