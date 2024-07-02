5e3e3d0e960d27bf505dc77222748d1e6cc8243e
HanzoDaBoss <hanif.uddz@gmail.com>
2024-07-02 20:36:12 +0100

    Changed responsive sizing, navbar headers, added basketball icon

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -15,119 +15,121 @@ export default function Leaderboard() {
   return (
     <>
       <header>Leaderboard</header>
-      <table className="min-w-full border border-neutral-200 text-center text-sm font-light text-surface dark:border-white/10 dark:text-white">
-        <thead className="border-b border-neutral-200 font-medium dark:border-white/10">
-          <tr>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              #
-            </th>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              Name
-            </th>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              IS
-            </th>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              MRS
-            </th>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              LRS
-            </th>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              PD
-            </th>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              ID
-            </th>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              PM
-            </th>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              RB
-            </th>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              BH
-            </th>
-            <th
-              scope="col"
-              className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
-            >
-              OVR
-            </th>
-          </tr>
-        </thead>
-        {playerList.map((player, index) => {
-          return (
-            <tbody>
-              <tr className="border-b border-neutral-200 dark:border-white/10 ">
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {index + 1}
-                </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {player.playerName}
-                </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {player.insideScoring}
-                </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {player.midRangeShooting}
-                </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {player.longRangeShooting}
-                </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {player.perimeterDefense}
-                </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {player.insideDefense}
-                </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {player.playmaking}
-                </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {player.rebound}
-                </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {player.ballHandling}
-                </td>
-                <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
-                  {Math.round(player.overallRating)}
-                </td>
-              </tr>
-            </tbody>
-          );
-        })}
-      </table>
+      <div class="inline-block min-w-full py-2 sm:px-6 lg:px-14">
+        <table className="min-w-full border border-neutral-200 text-center text-sm font-light text-surface dark:border-white/10 dark:text-white">
+          <thead className="border-b border-neutral-200 font-medium dark:border-white/10">
+            <tr>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                #
+              </th>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                Name
+              </th>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                IS
+              </th>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                MRS
+              </th>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                LRS
+              </th>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                PD
+              </th>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                ID
+              </th>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                PM
+              </th>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                RB
+              </th>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                BH
+              </th>
+              <th
+                scope="col"
+                className="border-e border-neutral-200 px-2 py-4 dark:border-white/10"
+              >
+                OVR
+              </th>
+            </tr>
+          </thead>
+          {playerList.map((player, index) => {
+            return (
+              <tbody>
+                <tr className="border-b border-neutral-200 dark:border-white/10 ">
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {index + 1}
+                  </td>
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {player.playerName}
+                  </td>
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {player.insideScoring}
+                  </td>
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {player.midRangeShooting}
+                  </td>
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {player.longRangeShooting}
+                  </td>
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {player.perimeterDefense}
+                  </td>
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {player.insideDefense}
+                  </td>
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {player.playmaking}
+                  </td>
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {player.rebound}
+                  </td>
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {player.ballHandling}
+                  </td>
+                  <td className="whitespace-nowrap border-e border-neutral-200 px-2 py-4 font-medium dark:border-white/10">
+                    {Math.round(player.overallRating)}
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      </div>
     </>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,10 +10,10 @@ import {
 import {Bars3Icon, BellIcon, XMarkIcon} from "@heroicons/react/24/outline";
 
 const navigation = [
-  {name: "Dashboard", href: "#", current: true},
-  {name: "Team", href: "#", current: false},
-  {name: "Projects", href: "#", current: false},
-  {name: "Calendar", href: "#", current: false},
+  {name: "Home", href: "#", current: true},
+  {name: "NBA News", href: "#", current: false},
+  {name: "Create", href: "#", current: false},
+  {name: "Admin", href: "#", current: false},
 ];
 
 function classNames(...classes) {
@@ -43,7 +43,7 @@ export default function Navbar() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    src="https://images.vexels.com/media/users/3/127117/isolated/svg/75741f0d611d334c461b2d184ead2c70.svg"
                     alt="Your Company"
                   />
                 </div>
@@ -68,15 +68,6 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
@@ -85,7 +76,7 @@ export default function Navbar() {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src="https://media.licdn.com/dms/image/C5603AQHvcjpFxhRHdg/profile-displayphoto-shrink_200_200/0/1621600701892?e=2147483647&v=beta&t=pzwqG7-YxuD8UCzoezXu5vE2x_3s_DwpoDmiLI0c5Yw"
                         alt=""
                       />
                     </MenuButton>
