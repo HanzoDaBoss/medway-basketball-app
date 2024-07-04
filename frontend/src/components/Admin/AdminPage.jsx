977c823f7bedd4806829701396b3ca2bb10230e3
HanzoDaBoss <hanif.uddz@gmail.com>
2024-07-04 22:22:52 +0100

    Added form for AddPlayer

--- frontend/src/components/Admin/AdminPage.jsx
+++ frontend/src/components/Admin/AdminPage.jsx
@@ -1,8 +1,11 @@
+import AddPlayer from "./AddPlayer";
+
 export default function AdminPage() {
   return (
-    <>
+    <div className="flex flex-col justify-center">
       <h1>ADMIN</h1>
       <h2>haha</h2>
-    </>
+      <AddPlayer />
+    </div>
   );
 }
