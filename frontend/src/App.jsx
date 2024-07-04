3786fc9a3d2e6cba8241609718771b62f88f7d0f
HanzoDaBoss <hanif.uddz@gmail.com>
2024-07-04 20:58:14 +0100

    Added new navbar for testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,12 @@ import "./App.css";
 import {Route, Routes} from "react-router-dom";
 import Leaderboard from "./components/Leaderboard";
 import Navbar from "./components/Navbar";
+import TestPage from "./components/TestPage";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <TestPage />
       <Routes>
         <Route path="/" element={<Leaderboard />}></Route>
       </Routes>
