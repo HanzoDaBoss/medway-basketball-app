007bf2e593da89e67caf997c0886547a86df34df
HanzoDaBoss <hanif.uddz@gmail.com>
2024-07-08 13:37:59 +0100

    Created and tested playerConverter function

--- frontend/src/utils/playerConverter.js
+++ frontend/src/utils/playerConverter.js
@@ -1,37 +1,39 @@
-function gradeConverter(player) {
+function playerConverter(player) {
   const grades = {
-    100: "A+",
-    95: "A",
-    90: "A-",
-    85: "B+",
-    82: "B",
-    79: "B-",
-    74: "C+",
-    72: "C",
-    69: "C-",
-    66: "D+",
-    62: "D",
-    58: "D-",
+    "A+": 100,
+    A: 95,
+    "A-": 90,
+    "B+": 85,
+    B: 82,
+    "B-": 79,
+    "C+": 74,
+    C: 72,
+    "C-": 69,
+    "D+": 66,
+    D: 62,
+    "D-": 58,
   };
 
   Object.entries(player).forEach(([attribute, value], index) => {
     if (index > 0 && index < 9) {
-      player[value] = grades[attribute];
+      player[attribute] = grades[value];
     }
   });
+
+  return player;
 }
 
 const testData = {
-  playerName: "Omor",
-  insideScoring: "A",
+  playerName: " Mr Test",
+  insideScoring: "A+",
   midRangeShooting: "A",
-  longRangeShooting: "B",
-  perimeterDefense: "A-",
-  insideDefense: "A",
-  playmaking: "A",
-  rebound: "A",
-  ballHandling: "A-",
-  multiplier: 1,
+  longRangeShooting: "A-",
+  perimeterDefense: "B-",
+  insideDefense: "C+",
+  playmaking: "D",
+  rebound: "D+",
+  ballHandling: "D-",
+  multiplier: 1.24,
 };
 
-export {gradeConverter};
+export {playerConverter};
