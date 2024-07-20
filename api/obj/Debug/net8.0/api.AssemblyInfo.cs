af18bd0d2f2e363bfbe731618ad775b13e6a85f2
HanzoDaBoss <hanif.uddz@gmail.com>
2024-07-20 12:44:23 +0100

    Deployed react app

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ed57c0af2e16cc220397784ec9a04869d3c2ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc708b984a64db8954582121ceb99ab7244872ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
