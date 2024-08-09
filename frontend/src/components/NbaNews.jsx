c9ab6be579a782d774bcbd144abb9c3e44e6e88f
HanzoDaBoss <hanif.uddz@gmail.com>
2024-08-09 17:26:47 +0100

    Mapped over articleList

--- frontend/src/components/NbaNews.jsx
+++ frontend/src/components/NbaNews.jsx
@@ -13,5 +13,22 @@ export default function () {
     });
   }, []);
 
-  return <></>;
+  return loading ? (
+    <div class="min-w-100 flex flex-col items-center">
+      <header>Leaderboard</header>
+      <div class="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+    </div>
+  ) : (
+    <div className="flex flex-col items-center">
+      {articleList.map((article) => {
+        return (
+          <>
+            <h2>{article.title}</h2>
+            <p>{article.url}</p>
+            <p>{article.source}</p>
+          </>
+        );
+      })}
+    </div>
+  );
 }
