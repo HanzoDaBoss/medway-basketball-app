e4f96833adc9088ddfb2322a095bb6449c36bd7e
HanzoDaBoss <hanif.uddz@gmail.com>
2024-08-09 17:19:31 +0100

    Created articleList state

--- frontend/src/components/NbaNews.jsx
+++ frontend/src/components/NbaNews.jsx
@@ -1,3 +1,6 @@
+import {useState} from "react";
+
 export default function () {
-  return <header>NBA News Coming Soon!</header>;
+  const [articleList, setArticleList] = useState([]);
+  return <></>;
 }
