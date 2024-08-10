2920efcd474ce87fb4a4d630e3661c13db901afa
HanzoDaBoss <hanif.uddz@gmail.com>
2024-08-10 18:07:41 +0100

    Hidden apiUrl using .env variables

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 const api = axios.create({
-  baseURL: `https://medway-basketball.azurewebsites.net/api`,
+  baseURL: apiUrl,
 });
 
 const nbaApi = axios.create({
