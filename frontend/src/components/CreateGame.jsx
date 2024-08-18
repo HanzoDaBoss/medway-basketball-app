ef08a6dd8cb7bb7a71f762311d3546cd8d0ea7b4
HanzoDaBoss <hanif.uddz@gmail.com>
2024-08-18 20:02:27 +0100

    Created loading state

--- frontend/src/components/CreateGame.jsx
+++ frontend/src/components/CreateGame.jsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 
 export default function CreateGame() {
   const [playerList, setPlayerList] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   return <header>Game Creator Coming Soon!</header>;
 }
