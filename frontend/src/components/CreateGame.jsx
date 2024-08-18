344ae33c79a81ce087d5fe4497e02cf3b6c783dc
HanzoDaBoss <hanif.uddz@gmail.com>
2024-08-18 21:18:43 +0100

    Created checkbox onclick handler function

--- frontend/src/components/CreateGame.jsx
+++ frontend/src/components/CreateGame.jsx
@@ -14,7 +14,19 @@ export default function CreateGame() {
     });
   }, []);
 
-  const handlePlayerForGame = () => {};
+  const handlePlayerForGame = (toggledPlayer) => {
+    setActivePlayers((currActivePlayers) => {
+      const playerIds = currActivePlayers.map((player) => player.id);
+      if (playerIds.includes(toggledPlayer.id)) {
+        return [...currActivePlayers].filter(
+          (player) => player.id !== toggledPlayer.id
+        );
+      } else {
+        return [...currActivePlayers, toggledPlayer];
+      }
+    });
+    console.log(activePlayers);
+  };
 
   return (
     <div class="flex flex-col items-center text-xl font-semibold">
@@ -24,6 +36,9 @@ export default function CreateGame() {
           <div class="flex items-center space-x-2 rounded p-2 hover:bg-gray-100 accent-teal-600">
             <input
               type="checkbox"
+              onChange={() => {
+                handlePlayerForGame(player);
+              }}
               id={player.id}
               name="languageCheckbox"
               class="h-4 w-4 rounded border-gray-300 text-teal-600 shadow-sm focus:border-teal-300 focus:ring focus:ring-teal-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400"
