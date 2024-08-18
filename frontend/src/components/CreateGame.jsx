a055fb9fcc278be84b012cba94fd1cecd13a70cb
HanzoDaBoss <hanif.uddz@gmail.com>
2024-08-18 20:04:32 +0100

    Created useEffect function

--- frontend/src/components/CreateGame.jsx
+++ frontend/src/components/CreateGame.jsx
@@ -1,8 +1,17 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {getPlayers} from "../api";
 
 export default function CreateGame() {
   const [playerList, setPlayerList] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getPlayers("overallRating").then((players) => {
+      setPlayerList(players);
+      setLoading(false);
+    });
+  }, []);
 
   return <header>Game Creator Coming Soon!</header>;
 }
