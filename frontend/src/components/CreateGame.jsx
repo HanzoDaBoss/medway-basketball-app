cf701bf9d563e2647e38552d6eeae4f0bc39436a
HanzoDaBoss <hanif.uddz@gmail.com>
2024-08-18 20:25:17 +0100

    Changed id of checkbox input to player id

--- frontend/src/components/CreateGame.jsx
+++ frontend/src/components/CreateGame.jsx
@@ -21,11 +21,11 @@ export default function CreateGame() {
           <div class="flex items-center space-x-2 rounded p-2 hover:bg-gray-100 accent-teal-600">
             <input
               type="checkbox"
-              id="htmlCheckbox"
+              id={player.id}
               name="languageCheckbox"
               class="h-4 w-4 rounded border-gray-300 text-teal-600 shadow-sm focus:border-teal-300 focus:ring focus:ring-teal-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400"
             />
-            <label for="htmlCheckbox" class="flex w-full space-x-2 text-sm">
+            <label for={player.id} class="flex w-full space-x-2 text-sm">
               {" "}
               {player.playerName}{" "}
             </label>
