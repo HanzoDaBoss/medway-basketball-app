b1c480c84e11bc088cd19afe7b56c35e5d685072
HanzoDaBoss <hanif.uddz@gmail.com>
2024-08-18 20:07:03 +0100

    Created base code for playerList checkboxes

--- frontend/src/components/CreateGame.jsx
+++ frontend/src/components/CreateGame.jsx
@@ -13,5 +13,45 @@ export default function CreateGame() {
     });
   }, []);
 
-  return <header>Game Creator Coming Soon!</header>;
+  return (
+    <div class="w-40 text-xl font-semibold">
+      <div class="flex items-center space-x-2 rounded p-2 hover:bg-gray-100 accent-teal-600">
+        <input
+          type="checkbox"
+          id="htmlCheckbox"
+          name="languageCheckbox"
+          class="h-4 w-4 rounded border-gray-300 text-teal-600 shadow-sm focus:border-teal-300 focus:ring focus:ring-teal-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400"
+        />
+        <label for="htmlCheckbox" class="flex w-full space-x-2 text-sm">
+          {" "}
+          HTML{" "}
+        </label>
+      </div>
+      <div class="flex items-center space-x-2 rounded p-2 hover:bg-gray-100 accent-teal-600">
+        <input
+          checked
+          type="checkbox"
+          id="cssCheckbox"
+          name="languageCheckbox"
+          class="h-4 w-4 rounded border-gray-300 text-teal-600 shadow-sm focus:border-teal-300 focus:ring focus:ring-teal-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400"
+        />
+        <label for="cssCheckbox" class="flex w-full space-x-2 text-sm">
+          {" "}
+          CSS{" "}
+        </label>
+      </div>
+      <div class="flex items-center space-x-2 rounded p-2 hover:bg-gray-100 accent-teal-600">
+        <input
+          type="checkbox"
+          id="javascriptCheckbox"
+          name="languageCheckbox"
+          class="h-4 w-4 rounded border-gray-300 text-teal-600 shadow-sm focus:border-teal-300 focus:ring focus:ring-teal-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400"
+        />
+        <label for="javascriptCheckbox" class="flex w-full space-x-2 text-sm">
+          {" "}
+          JavaScript{" "}
+        </label>
+      </div>
+    </div>
+  );
 }
